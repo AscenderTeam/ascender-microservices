8914c8490f1030d0716e8a4b3f74db61e266c55c
Zahcoder34 <65083166+Zahgrom34@users.noreply.github.com>
2024-12-29 18:26:38 +0500

    Consumer upd

--- ascender_ms/common/kafka/consume.py
+++
@@ -1,9 +0,0 @@
-class Consume:
-    def __init__(
-        self, 
-        topic: str | None = None,
-        key: bytes | None = None,
-        partition: int | None = None
-    ):
-        pass
-    
